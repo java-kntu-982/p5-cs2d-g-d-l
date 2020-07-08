a3d57d740f2d1ed9ef15821df549f8eebabe35f2
Danial Ch <danialchekani2000@gmail.com>
2020-07-08 15:45:12 +0430

    camera added

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+open module p.cs2d {
+    requires transitive javafx.web;
+    requires transitive javafx.controls;
+    requires transitive javafx.swing;
+    requires transitive javafx.graphics;
+}
